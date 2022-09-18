45b61d019f027d693473da86a0fe61d7a4519ea0
Joe Abraham <joseph.abraham2@outlook.com>
2022-09-18 13:41:24 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo docker network create ps-elastic
-sudo http.proxy="http://tstark:pssecrocks@172.31.245.222:8888" docker build -f /lab_security-network-analysis-using-zeek-logs/dockerfile_elastic -t "ps-elastic" .
-sudo http.proxy="http://tstark:pssecrocks@172.31.245.222:8888" docker build -f /lab_security-network-analysis-using-zeek-logs/dockerfile_kibana -t "ps-kibana" .
+sudo http.proxy="http://tstark:pssecrocks@172.31.245.222:8888" docker build -f /lab_security-log-analysis-using-a-siem/dockerfile_elastic -t "ps-elastic" .
+sudo http.proxy="http://tstark:pssecrocks@172.31.245.222:8888" docker build -f /lab_security-log-analysis-using-a-siem/dockerfile_kibana -t "ps-kibana" .
