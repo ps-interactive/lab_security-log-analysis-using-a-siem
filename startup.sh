f967f656ab03d24d6c3f69f73e7842a38c0d2989
Joe Abraham <joseph.abraham2@outlook.com>
2022-09-19 09:57:27 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,4 +3,4 @@ sudo docker network create ps-elastic
 sudo http.proxy="http://tstark:pssecrocks@172.31.245.222:8888" docker build -f /lab_security-log-analysis-using-a-siem/dockerfile_elastic -t "ps-elastic" .
 sudo http.proxy="http://tstark:pssecrocks@172.31.245.222:8888" docker build -f /lab_security-log-analysis-using-a-siem/dockerfile_kibana -t "ps-kibana" .
 sudo docker run -d --name ps-elastic --net ps-elastic -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" ps-elastic
-sudo docker run -d --name ps-kibana --net ps-elastic -p 5601:5601 -e "ELASTICSEARCH_HOSTS=http://ps-elastic:9200" ps-kibana
+sudo docker run -d --name ps-kibana --net ps-elastic -p 5601:5601 -e "ELASTICSEARCH_HOSTS=http://172.24.31.100:9200" ps-kibana
