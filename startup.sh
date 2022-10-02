1a0855c9e6374391267857ffb1b429f74a1bb451
Joe Abraham <joseph.abraham2@outlook.com>
2022-10-02 13:54:16 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,2 @@
-#!/bin/bash
-
-http.proxy="http://tstark:pssecrocks@172.31.245.222:8888" wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-sudo http.proxy="http://tstark:pssecrocks@172.31.245.222:8888" apt-get install -y apt-transport-https
-echo "deb https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-8.x.list
-sudo http.proxy="http://tstark:pssecrocks@172.31.245.222:8888" apt-get update && sudo http.proxy="http://tstark:pssecrocks@172.31.245.222:8888" apt-get install logstash
-
+http.host: "0.0.0.0"
+xpack.monitoring.elasticsearch.hosts: [ "http://ir-elasticsearch:9200" ]
